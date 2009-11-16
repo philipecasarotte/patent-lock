afcd9735d298e69ecb60261f60f2cf2cdc853a56
Marcio Trindade <marciotrindade@gmail.com>
2009-11-16 20:41:41 -0200

    add config.gem to google-checkout and money add js to more then one inventor

--- config/environment.rb
+++ config/environment.rb
@@ -26,6 +26,8 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "aws-s3", :lib => "aws/s3"
   # config.gem "thoughtbot-shoulda", :lib => "shoulda", :source => 'http://gems.github.com'
+  config.gem "topfunky-google-checkout", :lib => "google-checkout", :source => 'http://gems.github.com'
+  config.gem "money"
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
--- public/javascripts/questionnaire/step2.js
+++ public/javascripts/questionnaire/step2.js
@@ -1,4 +1,21 @@
 $(document).ready(function() {
+
+	var i = 0;
+	var steep_callback;
+	
+	$(".bt_add a").click(function(e) {
+		if ($(this).attr("onclick") != "") {
+			steep_callback = $(this).attr("onclick");
+			$(this).attr("onclick", "");
+		}
+		steep_callback();
+
+		$("#inventors_first_name, #inventors_last_name, #inventors_street_address, #inventors_state, #inventors_email, #inventors_middle_name, #inventors_citizenship, #inventors_city, #inventors_zipcode").each(function(index) {
+			$(this).attr("id", $(this).attr("id") + "_" + i).attr("name", $(this).attr("name") + "[]").prev("label").attr("for", $(this).attr("for") + "_" + i);
+		});
+		i++;
+	});
+
 	//Yes
 	$("#answer3_body_yes").click(function(){
 		$(".bt_add").hide();
