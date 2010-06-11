68588b8d77efe7a82a01404537e52d85e952d45c
Philipe Casarotte <phisoccer@globo.com>
2010-06-11 09:35:50 -0300

    add flash banner

--- public/javascripts/pages/home.js
+++ public/javascripts/pages/home.js
@@ -4,4 +4,12 @@ $(document).ready(function() {
 		window.open("http://patft.uspto.gov/netacgi/nph-Parser?Sect1=PTO2&Sect2=HITOFF&p=1&u=%2Fnetahtml%2FPTO%2Fsearch-bool.html&r=0&f=S&l=50&TERM1="+query+"&FIELD1=&co1=AND&TERM2=&FIELD2=&d=PTXT");
 		return false;
 	});
+	
+	$("#get_started_home").flashembed({
+		src:"/flashes/banner.swf",
+		width: "200px",
+		height: "281px",
+		allowscriptaccess: "always",
+		allowFullScreen: "true"
+	});
 });
